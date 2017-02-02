cb00b45c12a75b109a9f040ef1d138684fe49f5c
Robbert Wethmar <robbert@digitalica.nl>
2017-02-02 19:02:25 +0100

    Add test pag

---
+++ testpag.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robbert
+ * Date: 2-2-17
+ * Time: 18:46
+ */
+
+define('ABSPATH','');
+
+require('msxcomputermagazine.php');
+
+
+$issue = 57;
+
+$attr = array(
+    'mcm' => $issue
+);
+
+function plugin_dir_path($var) {
+    return dirname($var).'/';
+}
+
+function add_shortcode() {
+
+}
+
+$title = "nr. $issue - test";
+function get_the_title() {
+    return $title;
+}
+
+
+?>
+<html>
+<head>
+    <title><?php echo $title ?></title>
+</head>
+<body>
+<h1>testpag: MCM <?php echo $issue ?></h1>
+
+<?php
+
+echo mcm_pdf($attr);
+
+echo mcm_disk($attr);
+
+echo mcm_listings($attr);
+
+?>
+
+</body>
+</html>
