8f4dd177713533e11eeaacce4e2b316a43254729
Robbert Wethmar <robbert@digitalica.nl>
2017-02-11 00:51:12 +0100

    fix package

--- package.sh
+++ package.sh
@@ -5,5 +5,7 @@ echo
 rm -f msxcomputermagazine.zip
 zip msxcomputermagazine.zip msxcomputermagazine.php
 zip msxcomputermagazine.zip includes/mcmutils.php
+zip msxcomputermagazine.zip includes/mcmhtml.php
 zip msxcomputermagazine.zip includes/mcmlistings.php
+zip msxcomputermagazine.zip includes/mccmlistings.php
 unzip -l msxcomputermagazine.zip
