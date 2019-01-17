ef1f7ee7eb4d479b0954f2d58eab0a585101a062
Robbert Wethmar <robbert@digitalica.nl>
2019-01-17 12:22:39 +0100

    version nr bump

--- msxcomputermagazine.php
+++ msxcomputermagazine.php
@@ -2,7 +2,7 @@
 /*
 Plugin name: MSX Computer Magazine
 Description: Voor de links naar de listings, disks en pdfs van MSX Computer Magazine
-Version: 0.99
+Version: 1.01
 Author: Digitalica
 GitHub Plugin URI: https://github.com/digitalica/msxcomputermagazine
 License: GPL2
